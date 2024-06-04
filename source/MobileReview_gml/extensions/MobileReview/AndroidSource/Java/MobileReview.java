44c4f92bf727cf6c174a890f1d4314c624ed0766
Santi <darksacor@gmail.com>
2024-06-04 15:47:52 +0200

    Update Google Play Core libraries

--- source/MobileReview_gml/extensions/MobileReview/AndroidSource/Java/MobileReview.java
+++ source/MobileReview_gml/extensions/MobileReview/AndroidSource/Java/MobileReview.java
@@ -1,4 +1,6 @@
+
 package ${YYAndroidPackageName};
+
 import ${YYAndroidPackageName}.R;
 import com.yoyogames.runner.RunnerJNILib;
 
@@ -6,62 +8,52 @@ import android.app.Activity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.play.core.review.ReviewException;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.Task;
+import com.google.android.play.core.review.model.ReviewErrorCode;
 import com.google.android.play.core.review.ReviewInfo;
-import com.google.android.play.core.tasks.OnCompleteListener;
-import com.google.android.play.core.review.testing.FakeReviewManager;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.play.core.review.testing.FakeReviewManager;
 
 import android.util.Log;
 
-public class MobileReview extends RunnerSocial
-{
+public class MobileReview extends RunnerSocial {
 	int EVENT_OTHER_SOCIAL = 70;
 	
 	Activity activity = RunnerActivity.CurrentActivity;
  
-	public void MobileReview_Show()
-	{
-		final ReviewManager manager = ReviewManagerFactory.create(activity);
-		//final ReviewManager manager = new FakeReviewManager(activity);
+	public void MobileReview_Show()	{
 		
-		Task<ReviewInfo> request = manager.requestReviewFlow();
-		request.addOnCompleteListener(new OnCompleteListener<ReviewInfo>() 
-		{
-			@Override
-			public void onComplete(/*@NonNull*/ Task<ReviewInfo> task) 
-			{
-			   if (task.isSuccessful()) 
-				{
-					// We can get the ReviewInfo object
-					ReviewInfo reviewInfo = task.getResult();
-					final Task<Void> flow = manager.launchReviewFlow(activity,reviewInfo);
-					flow.addOnCompleteListener(new OnCompleteListener<Void>() 
-					{
-						@Override
-						public void onComplete(/*@NonNull*/ Task<Void> task) 
-						{
-							if (task.isSuccessful())
-							{
-								int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
-								RunnerJNILib.DsMapAddString( dsMapIndex, "type", "MobileReview_Show" );
-								RunnerJNILib.DsMapAddDouble( dsMapIndex, "success", 1.0);
-								RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, EVENT_OTHER_SOCIAL);
+        final ReviewManager manager = ReviewManagerFactory.create(activity);
+        // final ReviewManager manager = new FakeReviewManager(activity);
 
-							}
-							else 
-							{
-								int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
-								RunnerJNILib.DsMapAddString( dsMapIndex, "type", "MobileReview_Show" );
-								RunnerJNILib.DsMapAddDouble( dsMapIndex, "success", 0.0);
-								RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, EVENT_OTHER_SOCIAL);
-							}
-						}
-					});
-				}
-			}
-		});
+        Task<ReviewInfo> request = manager.requestReviewFlow();
+        request.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // We can get the ReviewInfo object
+                ReviewInfo reviewInfo = task.getResult();
+
+                final Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
+                flow.addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(/*@NonNull*/ Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
+                            RunnerJNILib.DsMapAddString( dsMapIndex, "type", "MobileReview_Show" );
+                            RunnerJNILib.DsMapAddDouble( dsMapIndex, "success", 1.0);
+                            RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, EVENT_OTHER_SOCIAL);
+
+                        } else {
+                            int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
+                            RunnerJNILib.DsMapAddString( dsMapIndex, "type", "MobileReview_Show" );
+                            RunnerJNILib.DsMapAddDouble( dsMapIndex, "success", 0.0);
+                            RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, EVENT_OTHER_SOCIAL);
+                        }
+                    }
+                });
+            }
+        });
 	}
 }
-
